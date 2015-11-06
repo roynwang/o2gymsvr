8bf23eece331bad94aa4aa0cea95199243ee075d
roynwang <royn.wang.renyuan@gmail.com>
2015-11-06 21:44:13 +0800

    modify favico

--- o2gym/urls.py
+++ o2gym/urls.py
@@ -3,18 +3,18 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'o2gym.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-	url(r'^favicon.ico$','django.views.generic.simple.redirect_to',{'url':'/static/images/favicon.ico'}),
-	url(r'',TemplateView.as_view(template_name="index.html")),
-	url(r'^api/protocol/$',TemplateView.as_view(template_name="agreement.html")),
-    url(r'^admin/', include(admin.site.urls)),
-	url(r'^', include('traincategory.urls')),
-    url(r'^', include('business.urls')),
-    url(r'^', include('weibo.urls')),
-    url(r'^', include('usr.urls')),
-    url(r'^', include('order.urls')),
-    url(r'^', include('recommend.urls')),
-    url(r'^', include('sms.urls')),
-]
+		# Examples:
+		# url(r'^$', 'o2gym.views.home', name='home'),
+		# url(r'^blog/', include('blog.urls')),
+	#	url(r'^favicon.ico$','django.views.generic.simple.redirect_to',{'url':'/static/images/favicon.ico'}),
+		url(r'^$',TemplateView.as_view(template_name="index.html")),
+		url(r'^api/protocol/$',TemplateView.as_view(template_name="agreement.html")),
+		url(r'^admin/', include(admin.site.urls)),
+		url(r'^', include('traincategory.urls')),
+		url(r'^', include('business.urls')),
+		url(r'^', include('weibo.urls')),
+		url(r'^', include('usr.urls')),
+		url(r'^', include('order.urls')),
+		url(r'^', include('recommend.urls')),
+		url(r'^', include('sms.urls')),
+		]
