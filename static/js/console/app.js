57c2b4bbc060c40c258d606688031484c276c31d
roynwang <royn.wang.renyuan@gmail.com>
2015-11-11 17:03:46 +0800

    add GMV summary

--- static/js/console/app.js
+++ static/js/console/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var app = angular.module('JobApp', [
-     'ui.router'
+     'ui.router',
+     'restangular'
  ])
- 
- app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, RestangularProvider) {
      // For any unmatched url, send to /route1
+	 RestangularProvider.setDefaultHeaders({Authorization: "JWT " + $.cookie("token") });
      $urlRouterProvider.otherwise("/");
      $stateProvider
          .state('index', {
@@ -16,7 +17,30 @@ var app = angular.module('JobApp', [
  
  })
  
- app.controller("MainPageCtrl", ['$scope',
- function ($scope) {
- 
+ app.controller("MainPageCtrl", ['$scope',"Restangular",
+ function ($scope, Restangular) {
+
+	 function renderCoaches(){
+		var gymid = $.cookie("gym")
+        Restangular.one('api/g/', gymid).get().then(function(gym){
+			$scope.coaches = gym.coaches_set
+			
+			$.each($scope.coaches, function(i,item){
+				console.log(i)
+				Restangular.one("api/",item.name).one("income/").get().then(function(data){
+					$scope.coaches[i].income = data
+				})
+			})
+			console.log($scope.coaches)
+		})
+		
+     }
+	 function recur(){
+		 if(!$.cookie("user")){
+			setTimeout(recur, 1000)
+		 }else {
+	 		renderCoaches()
+		}
+	 }
+	 recur()
  }])// end controller
