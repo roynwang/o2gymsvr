cd60bf6e92760808611c29b938637c847dc9eab7
roynwang <royn.wang.renyuan@gmail.com>
2015-11-22 15:33:56 +0800

    add forget password

--- console/urls.py
+++ console/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+		url(r'^console/forget/$',TemplateView.as_view(template_name="console/forget.html")),
 		url(r'^console/register/$',TemplateView.as_view(template_name="console/registration.html")),
 		url(r'^console/login/$',auth_views.login, {'template_name': 'console/login.html'}),
 		url(r'^console/dashboard/$', TemplateView.as_view(template_name="console/dashboard.html")),
