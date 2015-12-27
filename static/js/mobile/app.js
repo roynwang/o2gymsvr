8bca33c6c1d889ca1d7a490b4e11b7ffc8e323fc
roynwang <royn.wang.renyuan@gmail.com>
2015-12-28 01:04:07 +0800

    enlarge font

--- static/js/mobile/app.js
+++ static/js/mobile/app.js
@@ -226,7 +226,7 @@ app.controller("TodayCourseCtrl", ["$state", "$usersvc", "$date", "Restangular",
         that.refresh = function() {
             Restangular.one("api", user)
                 //.one("b", $date.seleteddate())
-                .one("b", new Date().Format("yyyyMMdd"))
+                .one("b", new Date().Format("20151201"))
                 .getList()
                 .then(function(data) {
                         that.courselist = data
