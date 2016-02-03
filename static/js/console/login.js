ffc13de9cc8c8fe0ef5685abaaadcaef6e7d8399
roynwang <royn.wang.renyuan@gmail.com>
2016-02-03 18:47:07 +0800

    templates/

--- static/js/console/login.js
+++ static/js/console/login.js
@@ -1,20 +1,38 @@
-$(function(){
-	$("#btn-login").click(function(){
-		var usrname = $("#phone").val()
-		var pwd = $("#pwd").val()
-		//validate the user
-		$.post("/api/lg/",{username:usrname, password:pwd}, function(data,status){
-			$.cookie("token",data.token,{ path: '/' })
-			$("#login-error").html("登录成功,2秒后跳转")
-			console.log($.cookie("token",data.token))
-			setTimeout(function(){
-			  window.location = "/console/dashboard/"
-			}, 2000)
-			//$("#login-error").html(data.token)
-		}).fail(function(resp){
-			if(resp.status == 403){
-				$("#login-error").html("错误的电话号码或密码")
-			}
-		})
-	})
+$(function() {
+    function login() {
+        var usrname = $("#phone").val()
+        var pwd = $("#pwd").val()
+            //validate the user
+        $.post("/api/lg/", {
+            username: usrname,
+            password: pwd
+        }, function(data, status) {
+            $.cookie("token", data.token, {
+                path: '/'
+            })
+            $("#login-error").html("登录成功,2秒后跳转")
+            console.log($.cookie("token", data.token))
+            setTimeout(function() {
+                    window.location = "/console/dashboard/"
+                }, 2000)
+                //$("#login-error").html(data.token)
+        }).fail(function(resp) {
+            if (resp.status == 403) {
+                $("#login-error").html("错误的电话号码或密码")
+            }
+        })
+    }
+
+    if ($.cookie("autologin")) {
+		window.location = "/console/dashboard/"
+	}
+
+    $("#btn-login").click(function() {
+        if (!$.cookie("autologin")) {
+            $.cookie("autologin", $("#autologin").val(), {
+                path: '/'
+            })
+        }
+		login()
+    })
 })
