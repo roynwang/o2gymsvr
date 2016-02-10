c9a69fb532aa9c4f6639c8ed50257c58df0b689e
roynwang <royn.wang.renyuan@gmail.com>
2016-02-11 02:37:57 +0800

    go

--- static/js/customermobile/app.js
+++ static/js/customermobile/app.js
@@ -8,6 +8,7 @@ var mainView = app.addView('.view-main');
 var pages = {
     "bookdetail": "/static/customermobile/book.html"
 }
+var TimeMap = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00"]
 
 $$(".isimg").on("click", function() {
     app.photoBrowser({
@@ -28,13 +29,15 @@ app.onPageInit('about', function(page) {
     // Loading flag
     var loading = false;
     // Last loaded index
-    var lastIndex = $$('.list-block li').length;
+    var lastIndex = $$('.list-block .monthview').length;
     // Max items to load
     var maxItems = 10;
     // Append items per load
     var itemsPerLoad = 4;
 
-    function bindHour() {
+    var expanding = undefined
+
+    function bindHour(tar) {
         function prepareSubmit(ele) {
             var btnp = $$('<p class="animated slideInRight" style="display:none"><span class="o2-book-submit">预约</span></p>')
             $$(ele).prepend(btnp)
@@ -71,10 +74,10 @@ app.onPageInit('about', function(page) {
         }
 
         function cleanSubmit() {
-            $$(".o2-book-hours ul li.booking>p").remove()
-            $$(".o2-book-hours ul li.booking").toggleClass("booking")
+            tar.find(".o2-book-hours ul li.booking>p").remove()
+            tar.find(".o2-book-hours ul li.booking").toggleClass("booking")
         }
-        $$(".o2-book-hours ul li").on("click", function(event) {
+        tar.find(".o2-book-hours ul li").on("click", function(event) {
             if (busy || $$(this).hasClass("booking")) return;
             cleanSubmit()
             $$(this).addClass("booking")
@@ -82,48 +85,65 @@ app.onPageInit('about', function(page) {
         })
     }
 
-    bindHour()
 
-	function buildMonthRow(arr){
-		var container = '<div class="accordion-item">' +
-                '<div class="accordion-item-toggle row no-gutter o2-book-days">'+
-	            '##</div>' +
-                '<div class="accordion-item-content">' +
-				'</div></div>'
-		var row = '<div class="col-auto"><p><span>##</span></p></div>'
-		var rows = ""
-		$$.each(arr, function(i, v){
-			rows += row.replace("##",v);
-		})
-		return container.replace("##", rows)
+    function buildMonthRow(arr) {
+        var container = '<div class="accordion-item">' +
+            '<div class="accordion-item-toggle row no-gutter o2-book-days">' +
+            '##</div>' +
+            '<div class="accordion-item-content">' +
+			'<div class="row no-gutter"><div class="col-100 o2-timeline-header-hr"></div></div>' +
+            '<div class="content-block o2-book-hours o2-bg-gradient"></div>' +
+            '</div></div>'
+        var row = '<div class="col-auto"><p><span>##</span></p></div>'
+        var rows = ""
+        $$.each(arr, function(i, v) {
+            rows += row.replace("##", v);
+        })
+        return container.replace("##", rows)
+    }
+
+    function buildMonth() {
+        var month = '<div class="monthview"><div class="content-block-title"><h2 style="margin:0px">十月</h2></div>' +
+            '<div class="content-block accordion-list custom-accordion o2-book-month">' +
+            '##</div>' +
+            '</div></div>';
+        var days = [1, 2, 3, 4, 5, 6, 7]
+        var weeks = ""
+        for (var i = 0; i < 4; i++) {
+            weeks += buildMonthRow(days)
+        }
+        return month.replace("##", weeks)
+    }
+
+    function loadmore() {
+        var months = buildMonth();
+        months += buildMonth();
+        months += buildMonth();
+        $$(".infinite-scroll .months").append(months)
+    }
+
+	function buildDayView() {
+		var rows = '<div class="list-block"><ul>##</ul></div>'
+		var hour = '<li class="item-content">'+
+                    	'<div class="item-media"><span class="hour">##</span><span class="min">00</span></div>' + 
+                       	 '<div class="item-inner"><div class="item-title"></div></div>' +
+                    '</li>' ;
+		var hourhalf = '<li class="item-content">'+
+                    	'<div class="item-media"><span class="hour hide"></span><span class="min">30</span></div>' + 
+                       	 '<div class="item-inner"><div class="item-title"></div></div>' +
+                    '</li>';
+		var alltext = ""
+		for(var i = 0; i < TimeMap.length; i++){
+			if(i%2 == 0){
+				alltext += hour.replace("##", 9 + i/2)
+			} else {
+				alltext += hourhalf
+			}
+		 }
+		return rows.replace("##", alltext);
 	}
-	function buildMonth(){
-		var month = '<div><div class="content-block-title"><h2 style="margin:0px">十月</h2></div>' +
-       		  '<div class="content-block accordion-list custom-accordion o2-book-month">' +
-			  '##</div>' + 
-			  '</div></div>';
-		var days = [1,2,3,4,5,6,7]
-		var weeks = ""
-		for(var i=0; i<4; i++){
-			weeks += buildMonthRow(days)
-		}
-		return month.replace("##", weeks)
-	}
 
-    var expanding = undefined
-
-
-
-	function loadmore(){
-		var months = buildMonth();
-		months += buildMonth();
-		months += buildMonth();
-		months += buildMonth();
-		months += buildMonth();
-		$$(".infinite-scroll .list-block").append(months)
-	}
-
-	loadmore()
+    loadmore()
 
     $$(".back").on("click", function() {
         mainView.router.back();
@@ -136,33 +156,32 @@ app.onPageInit('about', function(page) {
     });
     $$('.accordion-item').on('open', function(e) {
         expanding.addClass("active")
+		$$(this).find(".o2-book-hours").html(buildDayView());
+	    bindHour($$(this))
     });
     $$('.infinite-scroll').on('infinite', function() {
         // Exit, if loading in progress
         if (loading) return;
-
         // Set loading flag
         loading = true;
         // Emulate 1s loading
-        setTimeout(function() {
-            // Reset loading flag
-            loading = false;
-            if (lastIndex >= maxItems) {
-                // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
-                app.detachInfiniteScroll($$('.infinite-scroll'));
-                // Remove preloader
-                $$('.infinite-scroll-preloader').remove();
-                return;
-            }
-            // Generate new items HTML
+        // Reset loading flag
+        loading = false;
+        if (lastIndex >= maxItems) {
+            // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
+            app.detachInfiniteScroll($$('.infinite-scroll'));
+            // Remove preloader
+            $$('.infinite-scroll-preloader').remove();
+            return;
+        }
+        // Generate new items HTML
 
-            // Append new items
-            //$$('.list-block ul').append(html);
-			loadmore()
+        // Append new items
+        //$$('.list-block ul').append(html);
+        loadmore()
 
-            // Update last loaded index
-            lastIndex = $$('.list-block accordion-list').length;
-        }, 1000);
+        // Update last loaded index
+        lastIndex = $$('.list-block .monthview').length;
     });
 });
 
