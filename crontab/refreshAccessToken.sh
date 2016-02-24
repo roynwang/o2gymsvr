7576eba1ef32fd9247e47b4b388ca2be03e0c3d5
roynwang <royn.wang.renyuan@gmail.com>
2016-02-24 13:40:14 +0800

    set crontabl for wx access token

---
+++ crontab/refreshAccessToken.sh
@@ -0,0 +1,1 @@
+curl 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx72a88ef05d4fadc0&secret=629d00d5bbf75f8ca4728aee7c3feed2' > wxtoken
--- utils/wxutils.py
+++ utils/wxutils.py
@@ -1,10 +1,18 @@
 import requests
 from django.conf import settings
+import json
 
 def get_accesstoken():
+	f = open("../crontab/wxtoken","r")
+	line = f.read()
+	f.close()
+	return json.loads(line)["access_token"]
+
+	'''
 	params = {"grant_type":"client_credential",\
 			"appid":settings.WECHAT_APPID,\
 			"secret": settings.WECHAT_APPSECRET}
 	resp = requests.get(settings.WECHAT_TICKETURL,params)
 	return resp.json()["access_token"]
+	'''
 
