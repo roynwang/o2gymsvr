ebaba3e07ec2ecbcbd6a0eaaa64c19470502bdb0
roynwang <royn.wang.renyuan@gmail.com>
2016-03-18 16:23:15 +0800

    go

--- static/js/storesale/storehome.js
+++ static/js/storesale/storehome.js
@@ -232,7 +232,7 @@ var svc_gym = function() {
 
 
 app.onPageInit("gymhome", function(page) {
-    var basedate = new Date()
+    var basedate = new Date().addDays(1)
     var dates = [0, 0, 0, 0, 0, 0, 0]
     var selected = false
     var month = false
