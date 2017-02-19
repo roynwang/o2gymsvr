16042a36cf078e124ba880b2f5d186f449a1b7db
roynwang <royn.wang.renyuan@gmail.com>
2017-02-19 18:18:28 +0800

    update

--- o2gym/urls.py
+++ o2gym/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
 		url(r'^', include('sms.urls')),
 		url(r'^', include('fee.urls')),
 		url(r'^', include('coupon.urls')),
+		url(r'^', include('crawl.urls')),
 		url(r'^mobile/home/$', TemplateView.as_view(template_name="mobile/home.html")),
 		url(r'^mobile/login/$', TemplateView.as_view(template_name="mobile/login.html")),
 		url(r'^mobile/i/$', TemplateView.as_view(template_name="customermobile/home.html")),
