01dddc94d500b7b18e0fa4ed288580c59a88c421
roynwang <royn.wang.renyuan@gmail.com>
2017-02-26 20:48:36 +0800

    update

--- crontab/htmlcralwer.py
+++ crontab/htmlcralwer.py
@@ -10,7 +10,7 @@ def get_task(s):
     return resp.json()
 
 def crawl_html(s, url):
-    print url
+    print url.encode("utf-8")
     user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36'
     header = {"User-Agent": user_agent}
     res = s.get(url, headers=header)
