f0fa9ab4fb69406bdeb8f4143c32a1d989bfaefb
roynwang <royn.wang.renyuan@gmail.com>
2017-05-16 17:16:49 +0800

    update

--- doc/urls.py
+++ doc/urls.py
@@ -4,5 +4,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
 		url(r'^api/g/(?P<gymid>[0-9]+)/docs/$',GymDocList.as_view()),
+		url(r'^api/docs/(?P<pk>[0-9]+)/$',GymDocItem.as_view()),
 		url(r'^api/g/(?P<gymid>[0-9]+)/video/$',GymVideoList.as_view()),
+		url(r'^api/video/(?P<videoid>[0-9]+)/keywords/$',GymVideoKeywordList.as_view()),
+		url(r'^api/video/(?P<videoid>[0-9]+)/keywords/(?P<keyword>.+)/$',GymVideoKeywordItem.as_view()),
 		)
--- doc/views.py
+++ doc/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from rest_framework import generics
 from doc.serializers import *
@@ -10,8 +11,31 @@ class GymDocList(generics.ListCreateAPIView):
 	def get_queryset(self):
             return GymDoc.objects.filter(gym=self.kwargs['gymid'])
 
+class GymDocItem(generics.RetrieveUpdateDestroyAPIView):
+	serializer_class = GymDocSerializer
+        queryset = GymDoc.objects.all()
+
+
 class GymVideoList(generics.ListCreateAPIView):
 	serializer_class = GymVideoSerializer
         pagination_class = None
 	def get_queryset(self):
             return GymVideo.objects.filter(gym=self.kwargs['gymid'])
+
+
+class GymVideoKeywordList(generics.ListCreateAPIView):
+	serializer_class = GymVideoKeywordSerializer
+        pagination_class = None
+        def get_queryset(self):
+            return GymVideoKeyword.objects.filter(videoid=self.kwargs['videoid'])
+        
+
+class GymVideoKeywordItem(generics.RetrieveDestroyAPIView):
+        lookup_field = "keyword"
+	serializer_class = GymVideoKeywordSerializer
+        def get_queryset(self):
+            t = self.kwargs['keyword'].encode("utf8")
+            m =  GymVideoKeyword.objects.filter(keyword=t,videoid=self.kwargs['videoid'])
+            return m
+
+
